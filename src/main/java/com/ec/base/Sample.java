b768668b6118263df1e2b84615300ae023865947
amitbedre2394 <amitbedre2394@gmail.com>
2024-06-03 18:18:17 +0530

    architect asian

--- src/main/java/com/ec/base/Sample.java
+++ src/main/java/com/ec/base/Sample.java
@@ -1,8 +1,19 @@
 package com.ec.base;
 
+import org.testng.annotations.Test;
+
 public class Sample {
 	  public void m1() {
 		  System.out.println("firstmethod");
+	  }
+	  
+	  @Test
+	  public void git() {
+		  System.out.println("git hub");
+	  }
+	  @Test
+	  public void start() {
+		  System.out.println("test is starting");
 	  }
 
 }
